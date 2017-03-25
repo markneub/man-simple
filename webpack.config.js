ec7162de57ae71d88d94b027d48eb1d06cebb603
Mark Neuburger <markneub@gmail.com>
2017-03-25 17:17:18 -0400

    Initial commit with boilerplate (https://github.com/cvgellhorn/webpack-boilerplate)

---
+++ webpack.config.js
@@ -0,0 +1,110 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var dirApp = path.join(__dirname, 'app');
+var dirAssets = path.join(__dirname, 'assets');
+
+// Is the current build a development build
+var IS_DEV = (process.env.NODE_ENV === 'dev');
+
+module.exports = {
+    entry: {
+        vendor: [
+            'jquery',
+            'lodash'
+        ],
+        bundle: path.join(dirApp, 'index')
+    },
+    resolve: {
+        modules: [
+            'node_modules',
+            dirAssets,
+            dirApp
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            IS_DEV: IS_DEV
+        }),
+
+        new webpack.ProvidePlugin({
+            // jQuery
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            'root.jQuery': 'jquery',
+
+            // lodash
+            '_': 'lodash'
+        }),
+
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'index.ejs'),
+            title: 'Webpack Boilerplate'
+        })
+    ],
+    module: {
+        rules: [
+            // BABEL
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules)/,
+                options: {
+                    compact: true
+                }
+            },
+
+            // STYLES
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: IS_DEV
+                        }
+                    },
+                ]
+            },
+
+            // CSS / SASS
+            {
+                test: /\.scss/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: IS_DEV
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: IS_DEV,
+                            includePaths: [dirAssets]
+                        }
+                    }
+                ]
+            },
+
+            // EJS
+            {
+                test: /\.ejs$/,
+                loader: 'ejs-loader'
+            },
+
+            // IMAGES
+            {
+                test: /\.(jpe*g|png|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]'
+                }
+            }
+        ]
+    }
+};
