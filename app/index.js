437baf11f9a24b9cea877fbd4c34a360ea99cdda
Mark Neuburger <markneub@gmail.com>
2017-03-28 22:40:35 -0400

    Fix GA code

--- app/index.js
+++ app/index.js
@@ -3,10 +3,10 @@ import 'styles/index.scss';
 import './modal.js'
 
 // Google Analytics
-(function(m,a,r,c,u,s,){m['GoogleAnalyticsObject']=u;m[u]=m[u]||function(){
-(m[u].q=m[u].q||[]).push(arguments)},m[u].l=1*new Date();s=a.createElement(r),
-=a.getElementsByTagName(r)[0];s.async=1;s.src=c;.parentNode.insertBefore(s,)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+!function(M,a,r,k,N){M.GoogleAnalyticsObject=r;M[r]||(M[r]=function(){
+(M[r].q=M[r].q||[]).push(arguments)});M[r].l=+new Date;k=a.createElement('script');
+N=a.scripts[0];k.src='//www.google-analytics.com/analytics.js';
+N.parentNode.insertBefore(k,N)}(window,document,'ga');
 
 ga('create', 'UA-50553956-1', 'markneuburger.com');
 ga('send', 'pageview');
