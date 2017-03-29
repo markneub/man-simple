ffa6f8255ba2103607dd06d69ecb2366437b25d5
Mark Neuburger <markneub@gmail.com>
2017-03-28 22:33:54 -0400

    Add GA tracking code

--- app/index.js
+++ app/index.js
@@ -1,3 +1,12 @@
 import 'styles/index.scss';
 
 import './modal.js'
+
+// Google Analytics
+(function(m,a,r,c,u,s,){m['GoogleAnalyticsObject']=u;m[u]=m[u]||function(){
+(m[u].q=m[u].q||[]).push(arguments)},m[u].l=1*new Date();s=a.createElement(r),
+=a.getElementsByTagName(r)[0];s.async=1;s.src=c;.parentNode.insertBefore(s,)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-50553956-1', 'markneuburger.com');
+ga('send', 'pageview');
